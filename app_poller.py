548d207b6d167fc7eb8d5c73ea7ffd437a645393
Adrian VLADU <adyv_adyv@yahoo.com>
2013-08-24 18:29:24 +0000

    Added thread support

--- app_poller.py
+++ app_poller.py
@@ -1,18 +1,32 @@
 import requests
 import time
-import system
+from threading import Thread
+from cpu_info import *
+
 class AppPoller:
-	def __init__(self, base_url, endpoint = "/apps", timeout = 1):
+	cpu_info = CpuInfo()
+	stop_polling = False
+	
+	def __init__(self, base_url, endpoint = "/apps", timeout = 5):
 		self.base_url = base_url
 		self.endpoint = endpoint
 		self.timeout = timeout
-	def poll(self):
-		while True:
-			r = requests.get(self.base_url + self.endpoint).json()
+		self.serial_id = self.cpu_info.get_serial()
+	
+	def start(self):
+		thread = Thread(target = self.poll_thread)
+		thread.start()
+
+	def poll_thread(self):
+		full_url = self.base_url + self.endpoint + "/" + self.serial_id
+		while not self.stop_polling:
+			r = requests.get(full_url).json()
 			if len(r) > 0:
 				print "We found apps"
+				time.sleep(self.timeout)
 			else:
 				print "Sleeping"
 				time.sleep(self.timeout)
 		
-
+	def stop(self):
+		self. stop_polling = True
--- app_poller_client.py
+++ app_poller_client.py
@@ -1,7 +1,10 @@
+import time
 from app_poller import *
 
 url = "http://10.0.0.238:56653/api"
 endpoint = "/values"
 app_poller = AppPoller(url, endpoint)
-app_poller.poll()
+app_poller.start()
+time.sleep(10)
+app_poller.stop()
 
