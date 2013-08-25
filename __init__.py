42fc1262b5a8977c743f2010985cf9fba7d0e648
Adrian VLADU <adyv_adyv@yahoo.com>
2013-08-25 12:17:16 +0000

    Cleanup dirs

--- __init__.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-
-url = "http://10.0.0.238:56653/api/values";
-r = requests.get(url)
-print r.json()
--- app_downloader_client.py
+++
@@ -1,5 +0,0 @@
-from app_downloader import *
-
-app_id = "mail@google.com:pulamea"
-app_manager = AppManager(app_id)
-app_manager.install()
