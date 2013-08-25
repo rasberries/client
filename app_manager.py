a1bbd8ec2929df670adc1dda0e22dfa40f9eecdc
Adrian VLADU <adyv_adyv@yahoo.com>
2013-08-25 14:09:59 +0000

    Added install app and update status

--- app_manager.py
+++ app_manager.py
@@ -1,5 +1,7 @@
+from cpu_info import *
 import requests 
 import config as conf
+import json
 import random
 import os
 import subprocess
@@ -19,7 +21,7 @@ class AppManager:
 			r = requests.get(self.full_url)
                         if r.status_code < 300:
                         	 app = r.json()
-				 print app
+				 #print app
 				 return app
                         else:
                                  print "Error code %d" % r.status_code
@@ -33,7 +35,7 @@ class AppManager:
 				self.install_dependency(dep)
 		except Exception as e:
 			print "Failed to install app %s"  % e  
-		self.update()
+		self.update(app_info['id'])
 
 	def download_dependency(self, dep_url):
 		dep_folder = "%s/rasp-%s" %(self.base_path , random.randint(999,9999) )
@@ -45,18 +47,28 @@ class AppManager:
 
 	def install_dependency(self, dep):
 		dep_path = self.download_dependency(dep["url"])
-		bash_command = "file %s" % dep_path
-                process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-                output = process.communicate()[0]
-                print output
+		for cmd in dep["instructions"]:
+			cmd_txt = cmd["command"]
+			bash_command = "%s %s" % (cmd_txt, dep_path)
+			print bash_command
+                	process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+                	output = process.communicate()[0]
+                	print output
 
-
-	def update(self):
-		"devices/move_app"
-		{
-				uuid :"",
-				app_id:""
-		}
+	def update(self, app_id, update_endpoint = "devices/move_app" ):
+		update_url = "%s/%s" % (self.base_url,update_endpoint)
+		uuid = CpuInfo().get_serial()
+		update_data = {
+				'uuid' : uuid,
+				'app_id' : app_id
+				}
+		update_data_json = json.JSONEncoder().encode(update_data);
+		#update_data = '{"uuid" : "000000001b34f454","app_id" : "sabinmarcu@gmail.com$NPM"}'
+		#print update_data_json
+		r = requests.put(update_url, data = update_data_json, headers = {'Content-Type': 'application/json'})
+		if r.status_code <300:
+			pass
+			#dep = r.json()
 
 	def download_file(self, url, path):
 		dep_file = urllib2.urlopen(url)
